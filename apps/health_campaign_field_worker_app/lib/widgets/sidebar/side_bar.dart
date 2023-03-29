e91696882f21d8696c3fa7fb2ae76fa15bc34cb8
Shubhranshu Shekhar <shubhranshu.shekhar@thoughtworks.com>
2023-03-29 12:09:22 +0530

    Fixing side bar

--- apps/health_campaign_field_worker_app/lib/widgets/sidebar/side_bar.dart
+++ apps/health_campaign_field_worker_app/lib/widgets/sidebar/side_bar.dart
@@ -20,145 +20,150 @@ class SideBar extends StatelessWidget {
 
     return BlocBuilder<AuthBloc, AuthState>(builder: (context, state) {
       return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            color: theme.colorScheme.secondary.withOpacity(0.12),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-              child: state.maybeMap(
-                authenticated: (value) => Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      value.userModel.name.toString(),
-                      style: theme.textTheme.displayMedium,
-                    ),
+          Column(
+            children: [
+              Container(
+                color: theme.colorScheme.secondary.withOpacity(0.12),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                  child: state.maybeMap(
+                    authenticated: (value) => Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          value.userModel.name.toString(),
+                          style: theme.textTheme.displayMedium,
+                        ),
 
-                    // const SizedBox(
-                    //   height: 8,
-                    // ),
-                    Text(
-                      value.userModel.mobileNumber.toString(),
-                      style: theme.textTheme.labelSmall,
+                        // const SizedBox(
+                        //   height: 8,
+                        // ),
+                        Text(
+                          value.userModel.mobileNumber.toString(),
+                          style: theme.textTheme.labelSmall,
+                        ),
+                      ],
                     ),
-                  ],
+                    orElse: () => const Offstage(),
+                  ),
                 ),
-                orElse: () => const Offstage(),
               ),
-            ),
+              DigitIconTile(
+                title: AppLocalizations.of(context).translate(
+                  i18.common.coreCommonHome,
+                ),
+                icon: Icons.home,
+                onPressed: () {
+                  Navigator.of(context, rootNavigator: true).pop();
+                  context.router.replace(HomeRoute());
+                },
+              ),
+              // BlocBuilder<AppInitializationBloc, AppInitializationState>(
+              //   builder: (context, state) {
+              //     if (state is! AppInitialized) return const Offstage();
+              //
+              //     final appConfig = state.appConfiguration;
+              //     final languages = state.appConfiguration.languages;
+              //     final localizationModulesList =
+              //         state.appConfiguration.backendInterface;
+              //
+              //     return DigitIconTile(
+              //       title: AppLocalizations.of(context).translate(
+              //         i18.common.coreCommonlanguage,
+              //       ),
+              //       icon: Icons.language,
+              //       onPressed: () {
+              //         // TODO: Complete implementation
+              //       },
+              //       content: Offstage(
+              //         offstage: languages == null,
+              //         child: BlocBuilder<LocalizationBloc, LocalizationState>(
+              //           builder: (context, localizationState) {
+              //             return localizationModulesList != null
+              //                 ? Padding(
+              //                     padding: const EdgeInsets.only(top: 16),
+              //                     child: DigitRowCard(
+              //                       onChanged: (value) {
+              //                         int index = languages.indexWhere(
+              //                           (ele) =>
+              //                               ele.value.toString() ==
+              //                               value.value.toString(),
+              //                         );
+              //                         context
+              //                             .read<LocalizationBloc>()
+              //                             .add(LocalizationEvent.onLoadLocalization(
+              //                               module: localizationModulesList
+              //                                   .interfaces
+              //                                   .where((element) =>
+              //                                       element.type ==
+              //                                       Modules.localizationModule)
+              //                                   .map((e) => e.name.toString())
+              //                                   .join(',')
+              //                                   .toString(),
+              //                               tenantId:
+              //                                   appConfig.tenantId ?? "default",
+              //                               locale: value.value.toString(),
+              //                               path: Constants.localizationApiPath,
+              //                             ));
+              //
+              //                         context.read<LocalizationBloc>().add(
+              //                               OnUpdateLocalizationIndexEvent(
+              //                                 index: index,
+              //                                 code: value.value.toString(),
+              //                               ),
+              //                             );
+              //                       },
+              //                       rowItems: languages!.map((e) {
+              //                         var index = languages.indexOf(e);
+              //
+              //                         return DigitRowCardModel(
+              //                           label: e.label,
+              //                           value: e.value,
+              //                           isSelected:
+              //                               index == localizationState.index,
+              //                         );
+              //                       }).toList(),
+              //                       width: (MediaQuery.of(context).size.width *
+              //                               0.56 /
+              //                               languages.length) -
+              //                           (4 * languages.length),
+              //                     ),
+              //                   )
+              //                 : const Offstage();
+              //           },
+              //         ),
+              //       ),
+              //     );
+              //   },
+              // ),
+              // DigitIconTile(
+              //   title: 'Edit Profile',
+              //   icon: Icons.perm_contact_calendar,
+              //   onPressed: () {
+              //     // TODO: Complete implementation
+              //   },
+              // ),
+              // DigitIconTile(
+              //   title: 'Projects',
+              //   icon: Icons.folder,
+              //   onPressed: () {
+              //     // TODO: Complete implementation
+              //   },
+              // ),
+              DigitIconTile(
+                title: AppLocalizations.of(context)
+                    .translate(i18.common.coreCommonLogout),
+                icon: Icons.logout,
+                onPressed: () {
+                  context.read<AuthBloc>().add(AuthLogoutEvent());
+                },
+              ),
+            ],
           ),
-          DigitIconTile(
-            title: AppLocalizations.of(context).translate(
-              i18.common.coreCommonHome,
-            ),
-            icon: Icons.home,
-            onPressed: () {
-              Navigator.of(context, rootNavigator: true).pop();
-              context.router.replace(HomeRoute());
-            },
-          ),
-          // BlocBuilder<AppInitializationBloc, AppInitializationState>(
-          //   builder: (context, state) {
-          //     if (state is! AppInitialized) return const Offstage();
-          //
-          //     final appConfig = state.appConfiguration;
-          //     final languages = state.appConfiguration.languages;
-          //     final localizationModulesList =
-          //         state.appConfiguration.backendInterface;
-          //
-          //     return DigitIconTile(
-          //       title: AppLocalizations.of(context).translate(
-          //         i18.common.coreCommonlanguage,
-          //       ),
-          //       icon: Icons.language,
-          //       onPressed: () {
-          //         // TODO: Complete implementation
-          //       },
-          //       content: Offstage(
-          //         offstage: languages == null,
-          //         child: BlocBuilder<LocalizationBloc, LocalizationState>(
-          //           builder: (context, localizationState) {
-          //             return localizationModulesList != null
-          //                 ? Padding(
-          //                     padding: const EdgeInsets.only(top: 16),
-          //                     child: DigitRowCard(
-          //                       onChanged: (value) {
-          //                         int index = languages.indexWhere(
-          //                           (ele) =>
-          //                               ele.value.toString() ==
-          //                               value.value.toString(),
-          //                         );
-          //                         context
-          //                             .read<LocalizationBloc>()
-          //                             .add(LocalizationEvent.onLoadLocalization(
-          //                               module: localizationModulesList
-          //                                   .interfaces
-          //                                   .where((element) =>
-          //                                       element.type ==
-          //                                       Modules.localizationModule)
-          //                                   .map((e) => e.name.toString())
-          //                                   .join(',')
-          //                                   .toString(),
-          //                               tenantId:
-          //                                   appConfig.tenantId ?? "default",
-          //                               locale: value.value.toString(),
-          //                               path: Constants.localizationApiPath,
-          //                             ));
-          //
-          //                         context.read<LocalizationBloc>().add(
-          //                               OnUpdateLocalizationIndexEvent(
-          //                                 index: index,
-          //                                 code: value.value.toString(),
-          //                               ),
-          //                             );
-          //                       },
-          //                       rowItems: languages!.map((e) {
-          //                         var index = languages.indexOf(e);
-          //
-          //                         return DigitRowCardModel(
-          //                           label: e.label,
-          //                           value: e.value,
-          //                           isSelected:
-          //                               index == localizationState.index,
-          //                         );
-          //                       }).toList(),
-          //                       width: (MediaQuery.of(context).size.width *
-          //                               0.56 /
-          //                               languages.length) -
-          //                           (4 * languages.length),
-          //                     ),
-          //                   )
-          //                 : const Offstage();
-          //           },
-          //         ),
-          //       ),
-          //     );
-          //   },
-          // ),
-          // DigitIconTile(
-          //   title: 'Edit Profile',
-          //   icon: Icons.perm_contact_calendar,
-          //   onPressed: () {
-          //     // TODO: Complete implementation
-          //   },
-          // ),
-          // DigitIconTile(
-          //   title: 'Projects',
-          //   icon: Icons.folder,
-          //   onPressed: () {
-          //     // TODO: Complete implementation
-          //   },
-          // ),
-          DigitIconTile(
-            title: AppLocalizations.of(context)
-                .translate(i18.common.coreCommonLogout),
-            icon: Icons.logout,
-            onPressed: () {
-              context.read<AuthBloc>().add(AuthLogoutEvent());
-            },
-          ),
-          const PoweredByDigit(),
+              const PoweredByDigit(),
         ],
       );
     });
