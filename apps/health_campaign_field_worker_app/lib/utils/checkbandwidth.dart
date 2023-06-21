48723eaa5d4ad51bbc590c5a791678cc654d4e76
vishal <sarasani.vishalreddy@egovernments.org>
2023-06-21 12:59:31 +0530

    WIP auto sync

--- apps/health_campaign_field_worker_app/lib/utils/checkbandwidth.dart
+++ apps/health_campaign_field_worker_app/lib/utils/checkbandwidth.dart
@@ -165,7 +165,7 @@ void onStart(ServiceInstance service) async {
               flutterLocalNotificationsPlugin.show(
                 888,
                 'Auto Sync',
-                'Speed : ${speedArray.first.toString().substring(0, 4)}Mb/ps - BatchSize : $configuredBatchSize',
+                'Speed : ${speedArray.first.toString().substring(0, 1)}Mb/ps - BatchSize : $configuredBatchSize',
                 const NotificationDetails(
                   android: AndroidNotificationDetails(
                     "my_foreground",
