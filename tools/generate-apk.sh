a69115177c4ddbc3169f1ddee7851c633e6524ef
vishal <sarasani.vishalreddy@egovernments.org>
2023-06-27 12:13:22 +0530

    training apk changes

--- tools/generate-apk.sh
+++ tools/generate-apk.sh
@@ -1,34 +1,34 @@
-echo "Installing bricks"
-echo "-----------------"
-./tools/install_bricks.sh
-echo
+# echo "Installing bricks"
+# echo "-----------------"
+# ./tools/install_bricks.sh
+# echo
 
 cd apps/health_campaign_field_worker_app || exit
 
-echo "Creating env configs"
-echo "--------------------"
-cp -fr .env-dev .env
-echo
+# echo "Creating env configs"
+# echo "--------------------"
+# cp -fr .env-dev .env
+# echo
 
 rm -rf release-apk
 mkdir release-apk
 
-echo "Generating DEV APK"
-echo "-----------------"
-flutter build apk
-cp -fr build/app/outputs/flutter-apk/app-release.apk release-apk/apk-dev.apk
-echo
+# echo "Generating DEV APK"
+# echo "-----------------"
+# flutter build apk
+# cp -fr build/app/outputs/flutter-apk/app-release.apk release-apk/apk-dev.apk
+# echo
 
-echo "Creating env configs"
-echo "--------------------"
-cp -fr .env-uat .env
-echo
+# echo "Creating env configs"
+# echo "--------------------"
+# cp -fr .env-uat .env
+# echo
 
-echo "Generating UAT APK"
-echo "------------------"
-flutter build apk
-cp -fr build/app/outputs/flutter-apk/app-release.apk release-apk/apk-uat.apk
-echo
+# echo "Generating UAT APK"
+# echo "------------------"
+# flutter build apk
+# cp -fr build/app/outputs/flutter-apk/app-release.apk release-apk/apk-uat.apk
+# echo
 
 echo "Creating env configs"
 echo "--------------------"
