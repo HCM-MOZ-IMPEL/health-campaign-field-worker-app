0e6f95bf6bcf08abcfcad7102c54ca43f9384cdc
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-08-31 16:30:34 +0530

    Route to Delivery Intervention after Individual details recorded

--- apps/health_campaign_field_worker_app/lib/pages/beneficiary/deliver_intervention.dart
+++ apps/health_campaign_field_worker_app/lib/pages/beneficiary/deliver_intervention.dart
@@ -20,10 +20,8 @@ import '../../utils/environment_config.dart';
 import '../../utils/i18_key_constants.dart' as i18;
 import '../../utils/utils.dart';
 import '../../widgets/component_wrapper/product_variant_bloc_wrapper.dart';
-import '../../widgets/header/back_navigation_help_header.dart';
 import '../../widgets/localized.dart';
 import '../../widgets/showcase/config/showcase_constants.dart';
-import '../../widgets/showcase/showcase_button.dart';
 
 class DeliverInterventionPage extends LocalizedStatefulWidget {
   final bool isEditing;
@@ -87,11 +85,6 @@ class _DeliverInterventionPageState
                       form: () => buildForm(context),
                       builder: (context, form, child) {
                         return ScrollableContent(
-                          header: const Column(children: [
-                            BackNavigationHelpHeaderWidget(
-                              showcaseButton: ShowcaseButton(),
-                            ),
-                          ]),
                           footer: isDelivered
                               ? null
                               : DigitCard(
